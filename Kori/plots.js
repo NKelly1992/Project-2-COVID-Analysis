30d7355086bd5c7384db6d7ef889621c812fb07a
yankori <koriyan323@gmail.com>
2021-10-31 16:16:00 -0500

    add the radar chart

---
+++ Kori/plots.js
@@ -0,0 +1,27 @@
+data = [
+  {
+  type: 'scatterpolar',
+  r: [18958985, 61486935, 34664565, 31361205, 43802845, 18958985],
+  theta: ['Illinois','California','Florida', 'New York', 'Texas', 'Illinois'],
+  fill: 'toself',
+  name: 'Total vaccines distributed'
+  },
+  {
+  type: 'scatterpolar',
+  r: [6897684, 24045250, 12756457, 12876661, 15368246, 6897684],
+  theta: ['Illinois','California','Florida', 'New York', 'Texas', 'Illinois'],
+  fill: 'toself',
+  name: 'Total vaccinated'
+  }
+]
+
+layout = {
+  polar: {
+    radialaxis: {
+      visible: true,
+      range: [0, 60000000]
+    }
+  }
+}
+
+Plotly.newPlot("myDiv", data, layout)
